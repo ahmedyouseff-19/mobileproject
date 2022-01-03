4803915549604364251d3dbe774c0b46b0562f31
ahmed youseff <ahmed192107@fci.bu.edu.eg>
2022-01-03 13:12:35 +0200

    add log out button by ahmed youseff

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,9 +2,11 @@ import 'package:bfcai_hp/lec&sec_model/models.dart';
 import 'package:bfcai_hp/levels/level_2.dart';
 import 'package:bfcai_hp/levels/level_3.dart';
 import 'package:bfcai_hp/levels/level_4.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'levels/level_1.dart';
+
 // ِCreated by Ahmed Youseff
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,9 +16,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int currentIndex =0;
-  List<Widget> levels =
-  [
+  int currentIndex = 0;
+  List<Widget> levels = [
     Level1(),
     Level2(),
     Level3(),
@@ -26,19 +27,36 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading:IconButton(
-          color: Colors.white, onPressed: () {
-          setState(() {
-            Navigator.of(context)
-                .pushReplacementNamed('home');
-          });
-        },
-          icon: Icon(Icons.arrow_back,),
+        actions: [
+          TextButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+
+                setState(() {
+                  Navigator.of(context).pushReplacementNamed('login');
+                });
+              },
+              child: Text('LogOut',
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              ))
+        ],
+        leading: IconButton(
+          color: Colors.white,
+          onPressed: () {
+            setState(() {
+              Navigator.of(context).pushReplacementNamed('home');
+            });
+          },
+          icon: Icon(
+            Icons.arrow_back,
+          ),
         ),
-
-
         backgroundColor: myclr,
-        title:  Padding(
+        title: Padding(
           padding: const EdgeInsets.only(left: 70.0),
           child: Row(
             children: [
@@ -53,17 +71,14 @@ class _HomePageState extends State<HomePage> {
             ],
           ),
         ),
-
       ),
       body: levels[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
-        onTap: (index)
-        {
+        onTap: (index) {
           setState(() {
-            currentIndex=index;
+            currentIndex = index;
           });
-
         },
         type: BottomNavigationBarType.fixed,
         backgroundColor: myclr,
@@ -71,8 +86,7 @@ class _HomePageState extends State<HomePage> {
         unselectedItemColor: Colors.white,
         selectedFontSize: 18,
         elevation: 12,
-        items:
-        [
+        items: [
           BottomNavigationBarItem(
             icon: Icon(
               Icons.looks_one,
@@ -81,28 +95,27 @@ class _HomePageState extends State<HomePage> {
             label: 'level 1',
           ),
           BottomNavigationBarItem(
-              icon: Icon(
-                Icons.looks_two,
-                color: Colors.white,
-              ),
+            icon: Icon(
+              Icons.looks_two,
+              color: Colors.white,
+            ),
             label: 'level 2',
           ),
           BottomNavigationBarItem(
-              icon: Icon(
-                Icons.looks_3,
-                color: Colors.white,
-              ),
+            icon: Icon(
+              Icons.looks_3,
+              color: Colors.white,
+            ),
             label: 'level 3',
           ),
           BottomNavigationBarItem(
-              icon: Icon(
-                Icons.looks_4,
-                color: Colors.white,
-              ),
+            icon: Icon(
+              Icons.looks_4,
+              color: Colors.white,
+            ),
             label: 'level 4',
           ),
         ],
-
       ),
     );
   }
